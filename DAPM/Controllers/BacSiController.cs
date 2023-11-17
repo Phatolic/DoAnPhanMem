b224d5c701aa55096510e94afa1df0b67d02836a
Duy's <khanhduytran0508@gmail.com>
2023-11-17 16:58:09 +0700

    Bac si

---
+++ DAPM/Controllers/BacSiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAPM.Models;
+
+namespace DAPM.Controllers
+{
+    public class BacSiController : Controller
+    {
+        // GET: BacSi
+        DAPMEntities db = new DAPMEntities();
+        private List<BACSI> LayDSBacsi()
+        {
+            var dsBacsi = db.BACSIs.Include(bacsi => bacsi.CHUYENKHOA)
+                           .OrderByDescending(bacsi => bacsi.TenBS)
+                           .ToList();
+
+            return dsBacsi;
+        }
+
+        public ActionResult DSBacsi()
+        {
+            List<BACSI> dsBacsi = LayDSBacsi();
+
+            return PartialView(dsBacsi);
+        }
+    }
+}
