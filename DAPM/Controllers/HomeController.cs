bab10bfd4e0267c01be02e6d0c80d38ee010a4cf
Phat Pham <phatintowork@gmail.com>
2023-11-18 16:13:42 +0700

    b

--- DAPM/Controllers/HomeController.cs
+++ DAPM/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace DAPM.Controllers
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
